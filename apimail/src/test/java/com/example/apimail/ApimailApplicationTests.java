49fcab640ab1c5f48cdef996d5f2bb4885121968
BinduKotu15285 <bindustudy85@gmail.com>
2025-05-28 16:48:36 +0530

    Sql Mail Send1

---
+++ apimail/src/test/java/com/example/apimail/ApimailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apimail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sqlmail/src/main/java/com/example/sqlmail/SqlmailApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqlmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlmailApplication.class, args);
+	}
+
+}
